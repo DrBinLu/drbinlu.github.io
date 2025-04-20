1a73a28995cc6e9c39ec68b73dc894e6e8d0f205
DrBinLu <DrBinLu@gmail.com>
2025-04-20 12:16:35 +1000

    Add files via upload

--- scripts/fetch_tweets.py
+++ scripts/fetch_tweets.py
@@ -1,21 +1,27 @@
-import tweepy
 import os
+import tweepy
 
 api_key = os.getenv("TWITTER_API_KEY")
 api_secret = os.getenv("TWITTER_API_SECRET")
 access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_secret = os.getenv("TWITTER_ACCESS_SECRET")
 
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_secret)
-api = tweepy.API(auth)
+client = tweepy.Client(
+    consumer_key=api_key,
+    consumer_secret=api_secret,
+    access_token=access_token,
+    access_token_secret=access_secret
+)
 
-screen_name = "DrBinLu"
-tweet_count = 3
-tweets = api.user_timeline(screen_name=screen_name, count=tweet_count, tweet_mode="extended", exclude_replies=True, include_rts=False)
+user = client.get_user(username="DrBinLu").data
+tweets = client.get_users_tweets(id=user.id, max_results=5, tweet_fields=["text", "created_at"])
 
 with open("tweets.md", "w", encoding="utf-8") as f:
     f.write("## 🐦 Recent on X\n\n")
-    for tweet in tweets:
-        url = f"https://x.com/{screen_name}/status/{tweet.id}"
-        text = tweet.full_text.replace("\n", " ").strip()
-        f.write(f"- [{text}]({url})\n")
+    if not tweets.data:
+        f.write("*No recent tweets found.*\n")
+    else:
+        for tweet in tweets.data:
+            tweet_url = f"https://x.com/DrBinLu/status/{tweet.id}"
+            text = tweet.text.replace("\n", " ").strip()
+            f.write(f"- [{text}]({tweet_url})\n")
